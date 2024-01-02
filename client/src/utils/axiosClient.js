a595e14282e52f0689bab3e9cbceeb0829968264
Manish Sharma <sharmanish2109@gmail.com>
2024-01-02 20:21:16 +0530

    uncomment piece of code

--- client/src/utils/axiosClient.js
+++ client/src/utils/axiosClient.js
@@ -10,9 +10,9 @@ import { setLoading, showToast } from "../redux/slices/appConfigSlice";
 import { TOAST_FAILURE } from "../App";
 
 let baseURL = "http://localhost:4000/";
-// if (process.env.NODE_ENV === "production") {
-//   baseURL = process.env.REACT_APP_SERVER_BASE_URL;
-// }
+if (process.env.NODE_ENV === "production") {
+  baseURL = process.env.REACT_APP_SERVER_BASE_URL;
+}
 
 export const axiosClient = axios.create({
   baseURL,
